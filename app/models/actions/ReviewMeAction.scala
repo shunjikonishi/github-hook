727da086c5e88c5d5dab04d32389082637d495a8
shunjikonishi <shun.konishi@nifty.com>
2015-03-16 19:14:32 +0900

    Add debug log

--- app/models/actions/ReviewMeAction.scala
+++ app/models/actions/ReviewMeAction.scala
@@ -26,11 +26,13 @@ println("test1, " + isPullRequestOpen(msg) + ", " + isIssueComment(msg))
     }
 println("test2, " + text)
     text.split("\n").map(_.split("\t\\., ")).filter { words =>
+println("test3, " + words)
+println("test4, " + words.find(_.startsWith("@")) + ", " + words.find(_.equalsIgnoreCase("review")))
       words.find(_.startsWith("@")).isDefined && 
       words.find(_.equalsIgnoreCase("review")).isDefined
     }.headOption.foreach { words =>
       val reviewee = words.find(_.startsWith("@")).map(_.substring(1))
-println("test3, " + number + ", " + reviewee)
+println("test5, " + number + ", " + reviewee)
       api.addLabels(number, "Review me!")
       reviewee.foreach(api.assign(number, _))
     }
