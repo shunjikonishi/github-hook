29c21d52047ed02f5e328cf9b123e0327861fdbb
shunjikonishi <shun.konishi@nifty.com>
2015-03-16 19:09:31 +0900

    Add debug log

--- app/models/actions/ReviewMeAction.scala
+++ app/models/actions/ReviewMeAction.scala
@@ -16,6 +16,7 @@ class ReviewMeAction extends GitHubAction {
   private def isIssueComment(msg: GitHubEvent) = msg.name == "issue_comment"
 
   def isMatch(msg: GitHubEvent): Boolean = {
+println("test1, " + isPullRequestOpen(msg) + ", " + isIssueComment(msg))
     isPullRequestOpen(msg) || isIssueComment(msg)
   }
   def process(api: GitHubAPI, msg: GitHubEvent): Unit = {
@@ -23,11 +24,13 @@ class ReviewMeAction extends GitHubAction {
       case x: PullRequestEvent => (x.pull_request.body, x.pull_request.number)
       case x: IssueCommentEvent => (x.comment.body, x.issue.number)
     }
+println("test2, " + text)
     text.split("\n").map(_.split("\t\\., ")).filter { words =>
       words.find(_.startsWith("@")).isDefined && 
       words.find(_.equalsIgnoreCase("review")).isDefined
     }.headOption.foreach { words =>
       val reviewee = words.find(_.startsWith("@")).map(_.substring(1))
+println("test3, " + number + ", " + reviewee)
       api.addLabels(number, "Review me!")
       reviewee.foreach(api.assign(number, _))
     }
