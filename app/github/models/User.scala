addd277d14926ad3e5d3f23f4829dd86190616bf
shunjikonishi <shun.konishi@nifty.com>
2015-03-16 18:36:40 +0900

    Add debug log

--- app/github/models/User.scala
+++ app/github/models/User.scala
@@ -3,6 +3,7 @@ package github.models
 import play.api.libs.json.JsValue
 
 case class User(value: JsValue) {
+println("*******************" + play.api.libs.json.Json.prettyPrint(value))
   def id = (value \ "id").as[Long]
   def login = (value \ "login").as[String]
 }
