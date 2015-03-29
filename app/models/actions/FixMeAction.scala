15a6dfc9347baeb3f3351dbe2fcd22c3f64d5c36
shunjikonishi <shun.konishi@nifty.com>
2015-03-29 13:13:53 +0900

    Handle opener

--- app/models/actions/FixMeAction.scala
+++ app/models/actions/FixMeAction.scala
@@ -13,17 +13,21 @@ class FixMeAction extends GitHubAction {
     msg.name == "issue_comment"
   }
   def process(api: GitHubAPI, msg: GitHubEvent): Unit = {
-    val (text, number) = msg match {
-      case x: IssueCommentEvent => (x.comment.body, x.issue.number)
+    val (text, number, opener) = msg match {
+      case x: IssueCommentEvent => (
+        x.comment.body, 
+        x.issue.number,
+        x.issue.user.name
+      )
     }
     text.split("\n").map(_.split("[\t\\., ]")).filter { words =>
       val fix = words.indexOf("fix")
       val me = words.indexOf("me")
       fix >= 0 && me == fix + 1
     }.headOption.foreach { words =>
-      val assignee = words.find(_.startsWith("@")).map(_.substring(1))
+      val assignee = words.find(_.startsWith("@")).map(_.substring(1)).getOrElse(opener)
       api.addLabels(number, "Fix me!")
-      assignee.foreach(api.assign(number, _))
+      api.assign(number, assignee)
     }
   }
 }
